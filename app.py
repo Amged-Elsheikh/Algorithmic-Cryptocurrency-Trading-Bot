d1a60dd2bb9e93011b01573bbe6231185f0fa24f
Amged-Elsheikh <amgedelshiekh@gmail.com>
2023-02-12 01:35:53 +0900

    re-organize the run function

--- app.py
+++ app.py
@@ -2,25 +2,23 @@ from typing import *
 
 import dash
 import dash_bootstrap_components as dbc
-import plotly.express as px
-from dash import dcc, html
 
-from Connectors.binance_connectore import BinanceClient
-from dashboard.dashboard_ui import *
 from Moduls.data_modul import *
-
-# if TYPE_CHECKING:
-from dashboard.dashboard_callbacks import *
-
+from Connectors.binance_connectore import BinanceClient
 
 binance_client = BinanceClient(is_spot=False, is_test=True)
 
-exchanges = {"Binance":binance_client.contracts}
+clients = {'Binance': binance_client}
+exchanges = {"Binance": binance_client.contracts}
+
+from dashboard.dashboard_ui import *
+from dashboard.dashboard_callbacks import *
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div([contracts_layout(exchanges), 
-                      dcc.Interval(id='watchlist_interval', interval=1000)])
+app.layout = dbc.Container([contracts_layout(exchanges),
+                            strategy_layout(exchanges),
+                            dcc.Interval(id='watchlist_interval', interval=1000)])
 
 if __name__ == '__main__':
     app.run(debug=True)
