7f0653e5a965ad4ef13043030acc6fc2b2c7cf8e
Amged-Elsheikh <amgedelshiekh@gmail.com>
2023-05-21 14:10:20 +0900

    use type checking instead of importing the class

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from typing import Dict
+from typing import Dict, TYPE_CHECKING
 
 import dash_bootstrap_components as dbc
 from dash import Dash, dcc, html
 
 from Connectors.binance_connector import BinanceClient
-from Connectors.crypto_base_class import CryptoExchange
 from dashboard.dashboard_ui import (bottom_container, footer, middel_container,
                                     nav_bar, technical_modal, upper_container)
+if TYPE_CHECKING:
+    from Connectors.crypto_base_class import CryptoExchange
 
 clients = {'Binance': BinanceClient(is_spot=False, is_test=True)}
 
 
-def main(clients: Dict[str, CryptoExchange]):
+def main(clients: Dict[str, 'CryptoExchange']):
     external_stylesheets = [
         dbc.themes.BOOTSTRAP,
         dbc.icons.BOOTSTRAP,
